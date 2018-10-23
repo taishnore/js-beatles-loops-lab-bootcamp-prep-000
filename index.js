bd5521bda96472399521e773a32a4730fb62ead8
Taimur Shah <tshah@scu.edu>
2018-10-23 21:02:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+var beatlesLoops = []
+
+function theBeatlesPlay(musicians, instruments) {
+    for (var i = 0; i < musicians.length; i++) {
+        beatlesLoops.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+    return beatlesLoops
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return newFacts
+}
+
+
+
+
